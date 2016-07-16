8dda88f178e7336a6b8269894bd30d202f011a53
James Hopkins <jameshopkins0709@gmail.com>
2016-07-16 11:53:22 +0100

    Vars in their own file

--- main.tf
+++ main.tf
@@ -1,9 +1,3 @@
-variable "access_key_id" {}
-variable "secret_access_key" {}
-variable "webpagetest_api_key" { default = "sad43432reHH434dsad" }
-variable "region" { default = "eu-west-1" }
-variable "user_data" {}
-
 provider "aws" {
   region = "${var.region}"
   secret_key = "${var.secret_access_key}"
---
+++ variables.tf
@@ -0,0 +1,5 @@
+variable "access_key_id" {}
+variable "secret_access_key" {}
+variable "webpagetest_api_key" { default = "sad43432reHH434dsad" }
+variable "region" { default = "eu-west-1" }
+variable "user_data" {}
